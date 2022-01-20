0d1e123e7c52b68ec0a4839b7583bfb24eb70065
Cortez Christian <CortezChristian@rocketmortgage.com>
2022-01-20 17:11:28 -0500

    Added rest of code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from './country';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DevBuildAngularCountriesLab';
+  title = 'Hover to see country colors (Not actual Colors)';
+
+  countries: Country[] = [
+    new Country("Shrekadonia", "English", "", "Shrek", ["red", "orange", "blue"]),
+    new Country("Icantastan", "Icantaneese", "", "Jim", ["cyan", "purple", "blue"]),
+    new Country("Roman Empire", "Latin", "", "Julius Caesar", ["orange", "yellow", "green"])
+  ];
+
+  displayColors(index: number){
+    ( <HTMLElement> document.getElementById(`Country${index}`) ).style.border = `1px solid ${this.countries[index].colors[0]}`;
+    ( <HTMLElement> document.getElementById(`Country${index}`) ).style.backgroundColor = `${this.countries[index].colors[1]}`;
+    ( <HTMLElement> document.getElementById(`Country${index}`) ).style.color = `${this.countries[index].colors[2]}`;
+
+  }
+
+  defaultColors(index: number){
+    ( <HTMLElement> document.getElementById(`Country${index}`) ).style.border = "none";
+    ( <HTMLElement> document.getElementById(`Country${index}`) ).style.backgroundColor = "white";
+    ( <HTMLElement> document.getElementById(`Country${index}`) ).style.color = "black";
+  }
 }
